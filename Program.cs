f5f0d6ba68b3e09fc97da25a6a3d625feebd84b7
jeongjena <hgp7930@autuni.ac.nz>
2026-07-16 00:20:35 +1200

    Set up MSTest project for unit testing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ENSE707_Week1_Lab;
 
+BankAccount account = new BankAccount("Student User", 100);
+
+account.Deposit(50);
+account.Withdraw(30);
+
+Console.WriteLine($"Account Holder: {account.AccountHolder}");
+Console.WriteLine($"Current balance: {account.Balance}");
+Console.WriteLine($"Fee on $100 transaction: {account.CalculateTransactionFee(100)}");
